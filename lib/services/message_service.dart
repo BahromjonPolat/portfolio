dad21cffa3db3610b48fc0e86292e67695cc4695
BahromjonPolat <itjunior.uz@gmail.com>
2023-04-08 13:12:55 +0500

    Message model

---
+++ lib/services/message_service.dart
@@ -0,0 +1,14 @@
+/*
+
+  Created by: Bakhromjon Polat
+  Created on: Apr 08 2023 13:00:15
+  Github:   https://github.com/BahromjonPolat
+  Leetcode: https://leetcode.com/BahromjonPolat/
+  LinkedIn: https://linkedin.com/in/bahromjon-polat
+  Telegram: https://t.me/BahromjonPolat
+
+  Documentation: 
+
+*/
+
+class MessageService {}
